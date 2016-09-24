143435d3ae2d75108bec2171b43d31bc148f7096
wanghuiqin <1121802191@qq.com>
2016-09-24 13:40:01 +0800

    add

---
+++ PatProject/src/huiqin/yiji/FuermosDating.java
@@ -0,0 +1,10 @@
+package huiqin.yiji;
+/**
+ * 福尔摩斯约会问题
+ * 主要是基于条件的判断，先得到输入再判断输出
+ * @author huiqin @date 2016年9月24日 下午12:53:11
+ *
+ */
+public class FuermosDating {
+
+}
---
+++ PatProject/src/huiqin/yiji/PeopleGrade.java
@@ -0,0 +1,9 @@
+package huiqin.yiji;
+/**
+ * 德才题，利用不同方式，它的性能也不一样
+ * @author huiqin @date 2016年9月24日 下午12:58:53
+ *
+ */
+public class PeopleGrade {
+
+}
